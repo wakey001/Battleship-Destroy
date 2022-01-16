91906d0e1b9b6976a1830bb7e174d33530556834
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-16 12:28:12 +0000

    add fun ction print board

--- run.py
+++ run.py
@@ -4,7 +4,11 @@ import random
 
 board = []
 for i in range(5):
-    board.append(['O']*5)
-print(board)
-    
+    board.append(['O'] * 5)
+
+'Gets rid or the "" and inserts whitspace '
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
 
