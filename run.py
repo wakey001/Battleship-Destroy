10a1611ad333fd36c66a1bb1c0cba09ab8d6468b
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-16 17:23:22 +0000

    deleted code realised I needed a more complex while loop to also add the ability to tell the user if inputing a letter

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from random import randint
 
-turns = 5
+
 board = []
 # "O" = Capital letter o
 
 for i in range(8):
-    board.append(["O"] * 8)  # Appends 5 * "O" to the empty-list board
+    board.append(["O"] * 8)  # Appends 8  * "O" to the empty-list board
 # expected output["O", "O", "O", "O", "O"]
 
 
@@ -14,39 +14,9 @@ def print_board(board):
         print(" ".join(row))  # Gets rid or the "" and inserts whitspace
 
 
-def random_row(board):
-    return randint(0, len(board) - 1)  # Random integer within the board, Row
-
-
-def random_col(board):
-    return randint(0, len(board[0]) - 1)  # Random int within the board, Col
-
-
-# Assign both functions above to respective variables
-
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-guess_row = int(input("Guess Row: "))  # User to guess a Row 
-guess_col = int(input("Guess Col: "))  # User to guess a Col
-print(ship_row)
-print(ship_col)
-
-if guess_row == ship_row and guess_col == ship_col:
-    print("Well done you found and destroyed the battleship!")
-else:
-    if (guess_row < 0 or guess_row) > 4 or (guess_col < 0 and guess_col) > 4:
-        print("That co-ordinate is off the board please try again")
-    elif(board[guess_row][guess_col]) == "!":
-        print("Already guessed here, try again.")
-    else: 
-        print("You missed the battleship!")
-        board[guess_row][guess_col] = "!"
-    print_board(board)
-    
-
-
-
-
-    
-
+def create_ships():
+    for ship in range(5):
+        ship_row, ship_col = randint(0, 7), randint(0, 7)
+        while board[ship_row][ship_col] == "X":
+            ship_row, ship_col = randint(0, 7), randint(0, 7)
+        board[ship_row][ship_col] = "X"
