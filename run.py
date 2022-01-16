8d9ef6450333ef424107365ce212e0391de5ff68
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-16 16:12:24 +0000

    Made the board 8x8 with anticipation of adding more ships

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from random import randint
 
+turns = 5
 board = []
 # "O" = Capital letter o
 
-for i in range(5):
-    board.append(["O"] * 5)  # Appends 5 * "O" to the empty-list board
+for i in range(8):
+    board.append(["O"] * 8)  # Appends 5 * "O" to the empty-list board
 # expected output["O", "O", "O", "O", "O"]
 
 
@@ -30,10 +31,11 @@ guess_row = int(input("Guess Row: "))  # User to guess a Row
 guess_col = int(input("Guess Col: "))  # User to guess a Col
 print(ship_row)
 print(ship_col)
+
 if guess_row == ship_row and guess_col == ship_col:
-    print("Well done you found and destroyed the battleship!")  
+    print("Well done you found and destroyed the battleship!")
 else:
-    if guess_row < 0 or guess_row > 4 or guess_col < 0 and guess_col > 4:
+    if (guess_row < 0 or guess_row) > 4 or (guess_col < 0 and guess_col) > 4:
         print("That co-ordinate is off the board please try again")
     elif(board[guess_row][guess_col]) == "!":
         print("Already guessed here, try again.")
@@ -41,5 +43,10 @@ else:
         print("You missed the battleship!")
         board[guess_row][guess_col] = "!"
     print_board(board)
+    
 
+
+
+
+    
 
