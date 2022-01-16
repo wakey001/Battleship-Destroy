8c9240a8b44202af87b9d1b2665e2a6634a3419d
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-16 15:19:19 +0000

    Fix bug on line 33, changed guess_col to ship_col

--- run.py
+++ run.py
@@ -30,13 +30,15 @@ guess_row = int(input("Guess Row: "))  # User to guess a Row
 guess_col = int(input("Guess Col: "))  # User to guess a Col
 print(ship_row)
 print(ship_col)
-if guess_row == ship_row and guess_col == guess_col:
+if guess_row == ship_row and guess_col == ship_col:
     print("Well done you found and destroyed the battleship!")  
 else:
     if guess_row < 0 or guess_row > 4 or guess_col < 0 and guess_col > 4:
         print("That co-ordinate is off the board please try again")
-        elif(board[guess_row][guess_col]) == "!"
+    elif(board[guess_row][guess_col]) == "!":
+        print("Already guessed here, try again.")
     else: 
         print("You missed the battleship!")
+    print_board(board)
 
 
