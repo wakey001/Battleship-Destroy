71f1a01a367f0a5fb6030ab03d41434f32db9741
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-23 00:21:30 +0000

    added doc string to all functions

--- run.py
+++ run.py
@@ -1,27 +1,28 @@
 from random import randint
 
-
+# Represents boards that are currently empty
 board = []
 hidden_board = []
 users_board = []
 
 
-# "O" = Capital letter o
+# Will make a grid of 8x8 and append to list above
 for i in range(8):
     users_board.append(["U"] * 8)
 for i in range(8):
     hidden_board.append(["H"] * 8)
 for i in range(8):
-    board.append(["O"] * 8)  # Appends 8  * "O" to the empty-list board
-# expected output["O", "O", "O", "O", "O"]
+    board.append(["O"] * 8)  
 
 
 def print_board(board):
+    """Removes all quotes and instead adds blank space"""
     for row in board:
-        print(" ".join(row))  # Gets rid or the "" and inserts whitspace
+        print(" ".join(row))  
 
 
-def create_ships(board): 
+def create_ships(board):
+    """Generates 5 ships in random locations"""
     for ship in range(5):
         ship_row, ship_col = randint(0, 7), randint(0, 7)
         while board[ship_row][ship_col] == "X":
@@ -30,6 +31,7 @@ def create_ships(board):
 
 
 def create_user_ships(board):
+    """Generates 5 ships in random locations"""
     for ship in range(5):
         ship_row, ship_col = randint(0, 7), randint(0, 7)
         while users_board[ship_row][ship_col] == "X":
@@ -38,13 +40,15 @@ def create_user_ships(board):
 
 
 def computer_guess():
+    """Randomly guesses a row and col."""
     computer_row, computer_col = randint(1, 8), randint(1, 8)
     print("The computer guessed")
     print("row " + str(computer_row) + " "  "col " + str(computer_col))
     return int(computer_row) - 1, int(computer_col) - 1
 
 
-def get_ship_location(): 
+def get_ship_location():
+    """Asks user input for row,col."""
     row = input("Please enter a row between 1-8:\n")
     while row not in '1, 2, 3, 4, 5, 6, 7, 8':
         print("Error enter a number between 1-8")
@@ -57,6 +61,7 @@ def get_ship_location():
 
 
 def count_hit_ships(board):
+    """Iterates through the board to find "X"s"""
     count = 0
     for row in board:
         for col in row:
@@ -66,6 +71,7 @@ def count_hit_ships(board):
 
 
 def count_hit_user_ships(users_board):
+    """Iterates through the board to find "X"s"""
     user_count = 0
     for row in users_board:
         for col in row:
@@ -75,6 +81,7 @@ def count_hit_user_ships(users_board):
 
 
 def did_computer_hit():
+    """Tells the user if the computer hit them """
     computer_row, computer_col = computer_guess()
     if users_board[computer_row][computer_col] == "X":
         print("Computer hit a battleship you have \n")
@@ -82,9 +89,12 @@ def did_computer_hit():
     if users_board[computer_row][computer_col] == "U":
         print("computer missed ")
         users_board[computer_row][computer_col] = "-"
+    if count_hit_user_ships(users_board) == 5:
+        print("You lost all your battleships are destroyed")
 
 
 def get_user_name():
+    """Gets username input"""
     user_name = ""
     user_name = input("What is your name:\n")
     if user_name == "":
